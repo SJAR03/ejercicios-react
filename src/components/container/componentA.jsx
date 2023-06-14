ceaf856851cad621ff48dbe8a2b0f63e603e59e3
Sergio Ayerdis <72574282+SJAR03@users.noreply.github.com>
2023-06-14 02:00:48 -0600

    Completed the first exercise of the course about React from OpenBootCamp

---
+++ src/components/container/componentA.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ComponentB from '../pure/componentB'
+import { Contact } from '../../models/contact.class'
+
+const ComponentA = () => {
+
+    const defaultContact = new Contact('Sergio', 'Rodriguez', 'sayerdisrodriguez@gmail.com', false);
+
+  return (
+    <div>
+      <div>
+        <h1>
+            Contact Info:
+        </h1>
+      </div>
+      <ComponentB contact={defaultContact}></ComponentB>
+    </div>
+  )
+}
+
+export default ComponentA
