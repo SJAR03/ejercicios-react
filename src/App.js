dfd423d529cd9b3e6bbdf5b48e7af8ac34bda132
Sergio Ayerdis <72574282+SJAR03@users.noreply.github.com>
2023-06-21 02:46:55 -0600

    Oops

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-import Clock from './components/Clock';
-// import ComponentA from './components/container/componentA';
+import ComponentA from './components/ComponentA';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Clock></Clock>
+        <ComponentA></ComponentA>
       </header>
     </div>
   );
