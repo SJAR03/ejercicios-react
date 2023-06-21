00ef271d3692ec12ecc2e22bf1458ef7d9307869
Sergio Ayerdis Rodríguez <72574282+SJAR03@users.noreply.github.com>
2023-06-21 01:39:39 -0700

    Exercises 4 5 6 (#1)
    * Exercise 4, 5 and 6 from openBootCamp React course
    
    * Just edit something

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
-import ComponentA from './components/container/componentA';
+import Clock from './components/Clock';
+// import ComponentA from './components/container/componentA';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <ComponentA></ComponentA>
+        <Clock></Clock>
       </header>
     </div>
   );
