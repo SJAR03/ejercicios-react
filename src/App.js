b4a319f6878b1898975bcd32c110a240d1430d88
72574282+SJAR03@users.noreply.github.com <72574282+SJAR03@users.noreply.github.com>
2023-07-02 22:24:32 -0600

    All exercises

--- src/App.js
+++ src/App.js
@@ -1,15 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
-import Clock from './components/Clock';
-// import ComponentA from './components/container/componentA';
+
+/**
+ * Component for exercise 1-2-3
+ */
+import ComponentA from './Exercise-1-2-3/components/ComponentA';
+
+/**
+ * Component for exercise 4-5-6
+ */
+import Clock from './Exercise-4-5-6/components/Clock';
+
+/**
+ * Component for exercise 7-8-9
+ */
+import ContactList from './Exercise-7-8-9/components/contact_list';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Clock></Clock>
-      </header>
+    <div className='App'>
+      {/* Uncomment to run the component for exercise 1-2-3 */}
+      {/* <ComponentA></ComponentA> */}
+
+      {/* Uncomment to run the component for exercise 4-5-6 */}
+      {/* <Clock></Clock> */}
+
+      {/* Uncomment to run the component for exercise 7-8-9 */}
+      {/* <ContactList></ContactList> */}
     </div>
   );
 }
